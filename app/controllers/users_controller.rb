26171baae5f98e86338cbb59d34a7948aa081783
Tammy Metz <tammy@tammymetz.com>
2019-03-31 15:06:00 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,11 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
+  def create
+    User.create(user_params)
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
 end
