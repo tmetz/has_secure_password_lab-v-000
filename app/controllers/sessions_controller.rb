736aca3c7fe97c9cb87c94c2e764501ee8b66967
Tammy Metz <tammy@tammymetz.com>
2019-03-31 15:23:32 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def new
 
   end
-  
+
   def create
     @user = User.find_by(name: params[:user][:name])
     return head(:forbidden) unless @user.authenticate(params[:user][:password])
