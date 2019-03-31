e5af89f10764e99e571f93644fe12679d6b9f968
Tammy Metz <tammy@tammymetz.com>
2019-03-31 15:21:32 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190331151835) do
+ActiveRecord::Schema.define(version: 20190331152122) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
+    t.string   "name"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
